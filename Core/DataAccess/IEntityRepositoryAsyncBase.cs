d75f415a52b80b519c52e3b9d1754cfd71aded25
Oğuzhan <aydinayoguzhan@gmail.com>
2023-09-18 23:33:02 +0300

    IEntityRepository ve IEntityRepositoryBase interfaceleri ve EfEntityRepositoryBase classı tamamlandı

---
+++ Core/DataAccess/IEntityRepositoryAsyncBase.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepositoryAsyncBase<T> where T:class, IEntity, new()
+    {
+        Task<T> Get(Expression<Func<T, bool>> filter);
+        Task<IList<T>> GetList(Expression<Func<T,bool>> filter = null);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+    }
+}
