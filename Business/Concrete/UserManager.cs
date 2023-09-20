247f450457e5b333a9548992d96300cd6975027b
Oğuzhan <aydinayoguzhan@gmail.com>
2023-09-20 15:42:41 +0300

    User Business katmanı eklendi

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserManager: IUserService
+    {
+        private IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+    }
+}
