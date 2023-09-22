a5170d6fdeda8b21c9059723329ccd91ea6e9b91
Oğuzhan <aydinayoguzhan@gmail.com>
2023-09-22 16:59:13 +0300

    Register işlemi için validator eklendi

---
+++ Business/ValidationRules/FluentValidation/RegisterValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RegisterValidator: AbstractValidator<RegisterDto>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(u => u.Email).EmailAddress().NotNull().NotEmpty();
+            RuleFor(u => u.FirstName).NotNull().NotEmpty().MinimumLength(2);
+            RuleFor(u => u.LastName).NotNull().NotEmpty().MinimumLength(2);
+            RuleFor(u => u.Password).NotNull().NotEmpty().MinimumLength(6);
+        }
+    }
+}
